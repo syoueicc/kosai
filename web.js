cbf0bec05540d63f17c343e20ceb4944b760e100
cancel <you@example.comcancel.wu@foxmail.com>
2013-09-22 21:05:14 +0800

    post edt

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var http = require('http');
  
 var server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-type': 'text/html; charset:utf-8'});
+    res.writeHead(200, { 'Content-type': 'text/html; charset=utf-8'});
     res.write("最喜欢蔡蔡了~mua~");
     res.end();
 });
