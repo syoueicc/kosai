e17d5d07e6d6b0e2da38f8c98f2a6eba8f40b91f
cancel <you@example.comcancel.wu@foxmail.com>
2013-09-22 19:57:12 +0800

    post edt

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
-var fs = require('fs')
-    , http = require('http');
+var fs = require('fs'),
+	http = require('http');
  
 var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-type': 'text/html'});
     res.write("HELLO WORLD");
     res.end();
-}).listen(80, function() {
+}).listen(8080, function() {
     console.log('Listening at: http://localhost:80');
 });
