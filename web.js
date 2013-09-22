f862e9d4a88c4487d0851d268888d0d749b256bd
Cancel <mac@CC.local>
2013-09-22 17:22:36 +0800

    test

--- web.js
+++ web.js
@@ -5,6 +5,6 @@ var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-type': 'text/html'});
     res.write("HELLO WORLD");
     res.end();
-}).listen(80, function() {
+}).listen(8888, function() {
     console.log('Listening at: http://localhost:80');
 });
