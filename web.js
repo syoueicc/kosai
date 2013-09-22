fff3108fd7e593db26ee8a9d3cd76d1c4b635263
cancel <you@example.comcancel.wu@foxmail.com>
2013-09-22 20:45:34 +0800

    post edt

--- web.js
+++ web.js
@@ -4,6 +4,8 @@ var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-type': 'text/html'});
     res.write("HELLO WORLD");
     res.end();
-}).listen(8080, function() {
-    console.log('Listening at: http://localhost:8080');
+});
+var port = process.env.PORT || 3000;
+server.listen(port, function() {
+    console.log('Listening at: http://localhost: '+ port );
 });
