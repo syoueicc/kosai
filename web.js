0d1229486bf6472100e99a546d10c16552d680b1
Cancel <mac@CC.local>
2013-09-22 14:28:25 +0800

    myApp

---
+++ web.js
@@ -0,0 +1,10 @@
+var fs = require('fs')
+    , http = require('http');
+ 
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-type': 'text/html'});
+    res.write("HELLO WORLD");
+    res.end();
+}).listen(8080, function() {
+    console.log('Listening at: http://localhost:80');
+});
