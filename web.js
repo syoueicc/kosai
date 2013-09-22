d295c583b1a37efe56a26d93ea176c6820368eff
cancel <you@example.comcancel.wu@foxmail.com>
2013-09-22 20:51:33 +0800

    post edt

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var http = require('http');
  
 var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-type': 'text/html'});
-    res.write("HELLO WORLD");
+    res.write("最喜欢蔡蔡了~mua~");
     res.end();
 });
 var port = process.env.PORT || 3000;
