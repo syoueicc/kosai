08d5b2856e9551b86e6a780026b577a23e807834
cancelv5 <cancel.wu@foxmail.com>
2013-10-28 18:58:30 +0800

    add test model

--- public/script/app.js
+++ public/script/app.js
@@ -2,11 +2,12 @@ require.config({
     baseUrl: '/static/script/',
     paths: {
         jquery : 'lib/jquery.2.0.3',
-		less : 'lib/less-1.4.2'
+		less : 'lib/less-1.4.2',
+		bootstrap : '../libs/bootstrap-3.0.0-dist/js/bootstrap.min'
     }
 });
 
 
-require(['jquery','less'],function($, test){
+require(['jquery','less', 'bootstrap'],function($, less, bootstrap){
 	
 });
---
+++ test/js/test.js
@@ -0,0 +1,7 @@
+test( "hello test", function() {
+  ok( 1 == "1", "Passed!" );
+  ok(true, "test passed!");
+});
+test('user Test',function(){
+	ok(true == true, 'user Test passed!');
+});
